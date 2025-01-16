60a71bd8908b4c2340e1d3c9d0163fa8918fc0ea
Oscar Skean <ofskean727@gmail.com>
2025-01-16 12:16:16 -0500

    added more graphs

--- experiments/utils/misc/results_saving.py
+++ experiments/utils/misc/results_saving.py
@@ -1,7 +1,10 @@
 import os
 import pickle
 from typing import Any, Callable, List, Literal, Type, Dict, Union
-
+import json
+import mteb
+import pandas as pd
+import numpy as np
 from ..metrics.metric_calling import EvaluationMetricSpecifications
 from ..model_definitions.base_automodel_wrapper import BaseModelSpecifications
 
@@ -69,31 +72,95 @@ def load_results_for_model_and_revisions(model_family, model_size, revisions, ev
             results[(revision, evaluation_metric)] = load_results(model_specs, evaluation_metric_specs, dataloader_kwargs)
     return results
 
-def load_all_results():
+def load_all_results(should_normalize_scores_across_models: bool = False):
+    """
+    This is ugly code but it works. Basically it expects the folder structure to be as follows:
+
+
+    experiments/large_results/
+        model_family/
+            model_size/
+                revision/
+                    metrics/
+                        dataset_name/
+                            evaluation_metric.pkl
+                            ...
+                    mteb/
+                        layer_number/
+                            dataset_name.json
+                            ...
+
+    Arguments:
+        should_normalize_scores_across_models: If True, normalize scores to have the same mean and std at the model-revision-layer level.
+    """
     all_results = {}
     results_dir = BASE_PATH
 
+
+    # for each task, find the corresponding dataset name  
+    mteb_eng = mteb.get_benchmark("MTEB(eng)")
+    reduced_mteb_eng_tasks = [task for task in mteb_eng if task.metadata.category != 'p2p']
+    reduced_mteb_eng_tasks = [task for task in reduced_mteb_eng_tasks if task.metadata.type != 'Retrieval']
+    evaluator = mteb.MTEB(tasks=reduced_mteb_eng_tasks)
+    task_name_to_dataset_name = {}
+    for task in evaluator.tasks:
+        task_name_to_dataset_name[task.metadata.name] = task.metadata.dataset['path'].split('/')[1]
+    dataset_name_to_task_name = {v: k for k, v in task_name_to_dataset_name.items()}
+
+    # iterate over model families
+    vision_model_families = ['dinov2', 'dinov2-register', 'mae', 'clip', 'vit', 'i-jepa']
     for model_family in os.listdir(results_dir):
+        if model_family in vision_model_families:
+            continue
+
         model_family_path = os.path.join(results_dir, model_family)
         if os.path.isdir(model_family_path):
             all_results[model_family] = {}
             
+            # iterate over model sizes
             for model_size in os.listdir(model_family_path):
                 model_size_path = os.path.join(model_family_path, model_size)
                 if os.path.isdir(model_size_path):
                     all_results[model_family][model_size] = {}
                     
+                    # iterate over revisions
                     for revision in os.listdir(model_size_path):
                         revision_path = os.path.join(model_size_path, revision)
                         if os.path.isdir(revision_path):
                             all_results[model_family][model_size][revision] = {}
                             
-                            metrics_path = os.path.join(revision_path, "metrics")
+
+                            # load MTEB results
+                            mteb_path = os.path.join(revision_path, "mteb")
+                            if os.path.isdir(mteb_path):
+                                all_results[model_family][model_size][revision] = {}
+                                for layer in os.listdir(mteb_path):
+                                    layer_path = os.path.join(mteb_path, layer)
+                                    if os.path.isdir(layer_path):
+                                        all_results[model_family][model_size][revision][layer] = {}
+                                        for dataset_file in os.listdir(layer_path):
+                                            if dataset_file.endswith('.json') and 'model_meta' not in dataset_file:
+                                                file_path = os.path.join(layer_path, dataset_file)
+                                                with open(file_path, 'r') as f:
+                                                    task_results = json.load(f)
+
+
+                                                task_name = task_results['task_name']
+                                                main_score = task_results['scores']['test'][0]['main_score']
+                                                all_results[model_family][model_size][revision][layer][task_name] = {
+                                                    'main_score': main_score,
+                                                    'dataset_name': task_name_to_dataset_name[task_name]
+                                                }
+
+
+
+                            # load metrics
+                            metrics_path = os.path.join(revision_path, "metrics", "mteb")
+                
                             if os.path.isdir(metrics_path):
                                 for dataset in os.listdir(metrics_path):
-                                    dataset_path = os.path.join(metrics_path, dataset)
+                                    dataset_path = os.path.join(metrics_path,  dataset, "test")
                                     if os.path.isdir(dataset_path):
-                                        all_results[model_family][model_size][revision][dataset] = {}
                                         
                                         for metric_file in os.listdir(dataset_path):
                                             if metric_file.endswith('.pkl'):
@@ -101,6 +168,39 @@ def load_all_results():
                                                 file_path = os.path.join(dataset_path, metric_file)
                                                 with open(file_path, 'rb') as f:
                                                     metric_results = pickle.load(f)
-                                                all_results[model_family][model_size][revision][dataset][metric_name] = metric_results
 
+                                                # each metric pkl is a dictionary of lists of floats, one float per layer
+                                                # we need to put this into the already existing all_results structure
+                                                for metric_normalization, metric_values in metric_results.items():
+                                                    for layer, metric_value in enumerate(metric_values):
+                                                        correct_task_name = dataset_name_to_task_name[dataset]
+                                                        if dataset != 'wikitext':
+                                                            try:
+                                                                if not metric_name in all_results[model_family][model_size][revision][f"layer_{layer}"][correct_task_name]:
+                                                                    all_results[model_family][model_size][revision][f"layer_{layer}"][correct_task_name][metric_name] = {}
+                                                                all_results[model_family][model_size][revision][f"layer_{layer}"][correct_task_name][metric_name][metric_normalization] = metric_value
+                                                            except Exception as e:
+                                                                #print(f"Error: {e}", model_family, model_size, revision, f"layer_{layer}", correct_task_name, metric_name, metric_normalization, metric_value)
+                                                                pass
+   
+    if should_normalize_scores_across_models:
+        for model_family in all_results:
+            for model_size in all_results[model_family]:
+                for revision in all_results[model_family][model_size]:
+                    for layer in all_results[model_family][model_size][revision]:
+                        main_scores = [all_results[model_family][model_size][revision][layer][task_name]['main_score'] for task_name in all_results[model_family][model_size][revision][layer]]
+                        mean_main_score = np.mean(main_scores)
+                        std_main_score = np.std(main_scores)
+
+                        entropy_scores = [all_results[model_family][model_size][revision][layer][task_name]['entropy_sentence']['maxEntropy'] for task_name in all_results[model_family][model_size][revision][layer] if 'entropy_sentence' in all_results[model_family][model_size][revision][layer][task_name]]
+                        mean_entropy_score = np.mean(entropy_scores)
+                        std_entropy_score = np.std(entropy_scores)
+
+                        print(mean_entropy_score, std_entropy_score)    
+
+                        for task_name in all_results[model_family][model_size][revision][layer]:
+                            all_results[model_family][model_size][revision][layer][task_name]['standardized_main_score'] = (all_results[model_family][model_size][revision][layer][task_name]['main_score'] - mean_main_score) / std_main_score
+
+                            if 'entropy_sentence' in all_results[model_family][model_size][revision][layer][task_name]:
+                                all_results[model_family][model_size][revision][layer][task_name]['entropy_sentence']['standardized_logD'] = (all_results[model_family][model_size][revision][layer][task_name]['entropy_sentence']['maxEntropy'] - mean_entropy_score) / std_entropy_score
     return all_results
