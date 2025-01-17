c6c6ef8755fe128c2b8fd33eaf22bee314a80962
Oscar Skean <ofskean727@gmail.com>
2025-01-17 18:31:38 -0500

    added singularity def file

--- slurm_scripts/llm2vec-mteb-sweep.sh
+++ slurm_scripts/llm2vec-mteb-sweep.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-USE_SLURM=1
+USE_SLURM=0
 
 #MODEL_NAME="LLM2Vec-mntp-unsup-simcse"
 MODEL_NAME="LLM2Vec-mntp"
-MODEL_SIZES=('8B')
-MAX_LAYER=32
+MAX_LAYER=50
 REVISION="main"
+SIZE="8B"
 
-for size in ${MODEL_SIZES[@]}; do
-    for layer in $(seq 0 $MAX_LAYER); do
-        if [ $USE_SLURM -eq 1 ]; then
-            JOBNAME="llm2vec-$layer"
-            sbatch -J $JOBNAME slurm_submit.sh --model_family $MODEL_NAME --model_size $size --revision $REVISION --evaluation_layer $layer --purpose run_tasks
-        else
-            python MTEB-Harness.py \
-                --model_family $MODEL_NAME \
-                --model_size $size \
-                --revision $REVISION \
-                --evaluation_layer $layer \
-                --purpose run_tasks
-        fi
-    done
+for layer in $(seq 0 $MAX_LAYER); do
+    if [ $USE_SLURM -eq 1 ]; then
+        JOBNAME="llm2vec-$layer"
+        sbatch -J $JOBNAME slurm_submit.sh --model_family $MODEL_NAME --model_size $SIZE --revision $REVISION --evaluation_layer $layer --purpose run_tasks
+    else
+        python MTEB-Harness.py \
+            --model_family $MODEL_NAME \
+            --model_size $SIZE \
+            --revision $REVISION \
+            --evaluation_layer $layer \
+            --purpose run_tasks \
+            --raise_error True
+    fi
 done
